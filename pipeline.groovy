e5edee1cd76e69dd0e07df4fe51d0bb7eb9550e9
srisainaidu <srisainaidu444@gmail.com>
2023-06-01 11:26:31 +0530

    file modified

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,48 +2,18 @@ pipeline {
     agent any
     
     stages {
-        stage('Print Date 1') {
+        stage('Build Docker Image') {
             steps {
                 script {
-                    def date1 = new Date()
-                    println "Date : ${date1}"
-                }
-            }
-            post
-            {
-                always
-                {
-                    echo "stage 1 paased"
-                }
-            }
-        }
-        
-        stage('Print Date 2') {
-            steps {
-                script {
-                    def date2 = new Date()
-                    println "Date 2: ${date2}"
-                }
-            }
-        }
-        
-        stage('Print Date 3') {
-            steps {
-                script {
-                    def date3 = new Date()
-                    println "Date 3: ${date3}"
-                }
-            }
-        }
-        
-        stage('Print Date 4') {
-            steps {
-                script {
-                    def date4 = new Date()
-                    println "Date 4: ${date4}"
+                    def imageName = 'my-docker-image'
+                    def imageTag = 'latest'
+                    
+                    docker.withRegistry('https://docker.registry.url', 'docker-credentials-id') {
+                        def dockerImage = docker.build("${imageName}:${imageTag}", '.')
+                        dockerImage.push()
+                    }
                 }
             }
         }
     }
 }
-
